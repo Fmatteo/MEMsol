f91f97ebd181d28b31efb580ea5a01b33a091091
Fmatteo <markrecovery23@gmail.com>
2018-11-16 22:52:27 -0800

    natira

--- pages/cust_new.php
+++ pages/cust_new.php
@@ -34,36 +34,164 @@ endif;
   background:linear-gradient(#000, green);
   border-radius: 6px;
 }
-      .box-primary{
-        font-family: 'Comfortaa', cursive;
-        background-color: transparent;
-        border-radius: 15px;
-        margin-top: 5px;
-        border:1px solid black;
-        box-shadow: 2px 1px 200px 20px;
-        box-shadow: black;
-        color:black;
-        }
-
-        h3{
-          font-family: 'Comfortaa', cursive;
-          color:white;
-        }
-        .content-wrapper{
-        border-top-left-radius: 100px;
-        border-top-right-radius: 100px;
-      }
+     
     </style>
  </head>
   <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
-  <body class="hold-transition skin-<?php echo $_SESSION['skin'];?> layout-top-nav">
+  <body>
     <div class="wrapper">
       <?php include('../dist/includes/header.php');
       include('../dist/includes/dbcon.php');
       ?>
       <!-- Full Width Column -->
       <div class="content-wrapper">
-        <div class="container">
+        <aside class="main-sidebar" class="visible">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+          <!-- search form -->
+          <!-- sidebar menu: : style can be found in sidebar.less -->
+          <ul class="sidebar-menu">
+             <div id="sidebar-btn">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+     
+            <li class="treeview">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                      <i class="glyphicon glyphicon-refresh text-red"></i> Reorder
+                      <span class="label label-danger">
+                      <?php 
+                      $query=mysqli_query($con,"select COUNT(*) as count from product where prod_qty<=reorder and branch_id='$branch'")or die(mysqli_error());
+                      $row=mysqli_fetch_array($query);
+                      echo $row['count'];
+                      ?>  
+                      </span>
+                    </a>  
+              <ul class="treeview-menu">
+       <li class="header">You have <?php echo$row['count'];?> products that needs reorder</li>
+                      <li>
+                        <!-- Inner Menu: contains the notifications -->
+                        <ul class="menu">
+                        <?php
+                        $queryprod=mysqli_query($con,"select prod_name from product where prod_qty<=reorder and branch_id='$branch'")or die(mysqli_error());
+        while($rowprod=mysqli_fetch_array($queryprod)){
+      ?>
+                          <li><!-- start notification -->
+                            <a href="reorder.php">
+                              <i class="glyphicon glyphicon-refresh text-red"></i> <?php echo $rowprod['prod_name'];?>
+                            </a>
+                          </li><!-- end notification -->
+                          <?php }?>
+                        </ul>
+                      </li>
+                      <li class="footer"><a href="inventory.php">View all</a></li>
+                    </ul>
+                  </li>
+            <li class="treeview">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                      <i class="glyphicon glyphicon-wrench"></i> Maintenance
+                      
+                    </a>
+              <ul class="treeview-menu">
+       <li>
+                        
+              <li><!-- start notification -->
+                            <a href="category.php">
+                              <i class="glyphicon glyphicon-user text-green"></i> Company Name
+                            </a>
+                          </li><!-- end notification -->
+                          <li><!-- start notification -->
+                            <a href="customer.php">
+                              <i class="glyphicon glyphicon-user text-green"></i> Customer
+                            </a>
+                          </li><!-- end notification -->
+                          <li><!-- start notification -->
+                            <a href="creditor.php">
+                              <i class="glyphicon glyphicon-user text-green"></i> Credit Applicants
+                            </a>
+                          </li><!-- end notification -->
+              <li><!-- start notification -->
+                            <a href="product.php">
+                              <i class="glyphicon glyphicon-cutlery text-green"></i> Product
+                            </a>
+                          </li><!-- end notification -->
+             
+              <li><!-- start notification -->
+                            <a href="supplier.php">
+                              <i class="glyphicon glyphicon-send text-green"></i> Distributor
+                            </a>
+                          </li><!-- end notification -->
+                         
+             
+                        </ul>
+                      </li>
+                     
+                    
+                  </li>
+    <li class="treeview">
+      <a href="stockin.php">
+                      <i class="glyphicon glyphicon-list text-green"></i> Stock in/out
+                      
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li>
+                      </li>
+                     
+                    </ul>
+                  </li>
+    <li class="treeview">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                      <i class="glyphicon glyphicon-stats text-red"></i> Report
+                     
+                    </a>
+                   <ul class="treeview-menu">
+                     
+                          <li><!-- start notification -->
+                            <a href="inventory.php">
+                              <i class="glyphicon glyphicon-ok text-green"></i>Inventory
+                            </a>
+                          </li><!-- end notification -->
+                        <li><!-- start notification -->
+                         <a href="sales.php">
+                              <i class="glyphicon glyphicon-usd text-blue"></i>Sales
+                            </a>
+                          </li><!-- end notification -->
+              <li><!-- start notification -->
+                         <a href="receivables.php">
+                              <i class="glyphicon glyphicon-th-list text-redr"></i>Account Receivables
+                            </a>
+                          </li><!-- end notification -->
+              <li><!-- start notification -->
+                         <a href="income.php">
+                              <i class="glyphicon glyphicon-th-list text-redr"></i>Branch Income
+                            </a>
+                          </li><!-- end notification -->
+                          <li><!-- start notification -->
+                         <a href="purchase_request.php">
+                              <i class="glyphicon glyphicon-usd text-blue"></i>Purchase Request
+                            </a>
+                          </li><!-- end notification -->
+                        </ul>
+                      </li>
+                    
+    <li class="treeview">
+      <a href="profile.php" class="dropdown-toggle">
+                      <i class="glyphicon glyphicon-cog text-orange"></i>
+                      <?php echo $_SESSION['name'];?>
+                    </a>
+                  </li>
+
+    <li class="treeview">
+       <a href="logout.php" class="dropdown-toggle">
+                      <i class="glyphicon glyphicon-off text-red"></i> Logout 
+                      
+                    </a>
+                  </li>       
+          </ul>
+        </section>
+        <!-- /.sidebar -->
+      </aside>
           <!-- Content Header (Page header) -->
           <section class="content-header">
             <h1>
