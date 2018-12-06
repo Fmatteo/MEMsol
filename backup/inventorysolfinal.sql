3c55db3b57574f79bc5a3825255e17ddcab77e95
Fmatteo <markrecovery23@gmail.com>
2018-12-07 06:32:52 +0800

    backup

---
+++ backup/inventorysolfinal.sql
@@ -0,0 +1,523 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 06, 2018 at 11:31 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventorysol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branch`
+--
+
+CREATE TABLE `branch` (
+  `branch_id` int(11) NOT NULL,
+  `branch_name` varchar(50) NOT NULL,
+  `branch_address` varchar(100) NOT NULL,
+  `branch_contact` varchar(50) NOT NULL,
+  `skin` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `branch`
+--
+
+INSERT INTO `branch` (`branch_id`, `branch_name`, `branch_address`, `branch_contact`, `skin`) VALUES
+(6, 'Sols Agrivet Supply', 'Bongabong Oriental Mindoro', '09', 'red');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `cust_id` int(11) NOT NULL,
+  `cust_first` varchar(50) NOT NULL,
+  `cust_last` varchar(30) NOT NULL,
+  `cust_address` varchar(100) NOT NULL,
+  `cust_contact` varchar(30) NOT NULL,
+  `balance` decimal(10,2) NOT NULL,
+  `cust_pic` varchar(300) NOT NULL,
+  `bday` date NOT NULL,
+  `nickname` varchar(30) NOT NULL,
+  `house_status` varchar(30) NOT NULL,
+  `years` varchar(20) NOT NULL,
+  `rent` varchar(10) NOT NULL,
+  `emp_name` varchar(100) NOT NULL,
+  `emp_no` varchar(30) NOT NULL,
+  `emp_address` varchar(100) NOT NULL,
+  `emp_year` varchar(10) NOT NULL,
+  `occupation` varchar(30) NOT NULL,
+  `salary` varchar(30) NOT NULL,
+  `spouse` varchar(30) NOT NULL,
+  `spouse_no` varchar(30) NOT NULL,
+  `spouse_emp` varchar(50) NOT NULL,
+  `spouse_details` varchar(100) NOT NULL,
+  `spouse_income` decimal(10,2) NOT NULL,
+  `comaker` varchar(30) NOT NULL,
+  `comaker_details` varchar(100) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  `credit_status` varchar(10) NOT NULL,
+  `ci_remarks` varchar(1000) NOT NULL,
+  `ci_name` varchar(50) NOT NULL,
+  `ci_date` date NOT NULL,
+  `payslip` int(11) NOT NULL,
+  `valid_id` int(11) NOT NULL,
+  `cert` int(11) NOT NULL,
+  `cedula` int(11) NOT NULL,
+  `income` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses`
+--
+
+CREATE TABLE `expenses` (
+  `exp_id` int(10) NOT NULL,
+  `exp_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expensesinput`
+--
+
+CREATE TABLE `expensesinput` (
+  `expensesinput_id` int(11) NOT NULL,
+  `exp_name` varchar(255) NOT NULL,
+  `qty` int(6) NOT NULL,
+  `amount` decimal(13,2) NOT NULL,
+  `date` datetime NOT NULL,
+  `branch_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history_log`
+--
+
+CREATE TABLE `history_log` (
+  `log_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `action` varchar(100) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `history_log`
+--
+
+INSERT INTO `history_log` (`log_id`, `user_id`, `action`, `date`) VALUES
+(209, 10, 'has logged in the system at ', '2018-12-07 06:28:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL,
+  `cust_id` int(11) NOT NULL,
+  `sales_id` int(11) NOT NULL,
+  `payment` decimal(10,2) NOT NULL,
+  `payment_date` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  `payment_for` date NOT NULL,
+  `due` decimal(10,2) NOT NULL,
+  `interest` decimal(10,2) NOT NULL,
+  `remaining` decimal(10,2) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `rebate` decimal(10,2) NOT NULL,
+  `or_no` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `prod_id` int(11) NOT NULL,
+  `prod_name` varchar(100) NOT NULL,
+  `prod_desc` varchar(500) NOT NULL,
+  `prod_price` decimal(10,2) NOT NULL,
+  `prod_pic` varchar(300) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `prod_qty` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  `reorder` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `serial` varchar(50) NOT NULL,
+  `base_price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_request`
+--
+
+CREATE TABLE `purchase_request` (
+  `pr_id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `request_date` date NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  `purchase_status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `sales_id` int(11) NOT NULL,
+  `cust_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cash_tendered` decimal(10,2) DEFAULT NULL,
+  `discount` decimal(10,2) DEFAULT NULL,
+  `amount_due` decimal(10,2) NOT NULL,
+  `cash_change` decimal(10,2) DEFAULT NULL,
+  `date_added` datetime NOT NULL,
+  `modeofpayment` varchar(15) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  `total` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_details`
+--
+
+CREATE TABLE `sales_details` (
+  `sales_details_id` int(11) NOT NULL,
+  `sales_id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stockin`
+--
+
+CREATE TABLE `stockin` (
+  `stockin_id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL,
+  `qty` int(6) NOT NULL,
+  `date` datetime NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  `base_price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `supplier_id` int(11) NOT NULL,
+  `supplier_name` varchar(100) NOT NULL,
+  `supplier_address` varchar(300) NOT NULL,
+  `supplier_contact` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp_trans`
+--
+
+CREATE TABLE `temp_trans` (
+  `temp_trans_id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `term`
+--
+
+CREATE TABLE `term` (
+  `term_id` int(11) NOT NULL,
+  `sales_id` int(11) DEFAULT NULL,
+  `payable_for` varchar(10) NOT NULL,
+  `term` varchar(11) NOT NULL,
+  `due` decimal(10,2) NOT NULL,
+  `payment_start` date NOT NULL,
+  `down` decimal(10,2) NOT NULL,
+  `due_date` date NOT NULL,
+  `interest` decimal(10,2) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `branch_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password`, `name`, `status`, `branch_id`) VALUES
+(6, 'administrator', 'a1Bz20ydqelm8m1wql21232f297a57a5a743894a0e4a801fc3', 'Giu Matthew', 'active', 0),
+(8, 'admin', 'a1Bz20ydqelm8m1wql21232f297a57a5a743894a0e4a801fc3', 'Mark', 'active', 5),
+(9, 'admin', 'a1Bz20ydqelm8m1wql21232f297a57a5a743894a0e4a801fc3', 'Michael', 'active', 1),
+(10, 'admin', 'a1Bz20ydqelm8m1wql21232f297a57a5a743894a0e4a801fc3', 'Encoder', 'active', 6);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `branch`
+--
+ALTER TABLE `branch`
+  ADD PRIMARY KEY (`branch_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`) USING BTREE;
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cust_id`);
+
+--
+-- Indexes for table `expenses`
+--
+ALTER TABLE `expenses`
+  ADD PRIMARY KEY (`exp_id`,`exp_name`) USING BTREE;
+
+--
+-- Indexes for table `expensesinput`
+--
+ALTER TABLE `expensesinput`
+  ADD PRIMARY KEY (`expensesinput_id`);
+
+--
+-- Indexes for table `history_log`
+--
+ALTER TABLE `history_log`
+  ADD PRIMARY KEY (`log_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`prod_id`);
+
+--
+-- Indexes for table `purchase_request`
+--
+ALTER TABLE `purchase_request`
+  ADD PRIMARY KEY (`pr_id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`sales_id`);
+
+--
+-- Indexes for table `sales_details`
+--
+ALTER TABLE `sales_details`
+  ADD PRIMARY KEY (`sales_details_id`);
+
+--
+-- Indexes for table `stockin`
+--
+ALTER TABLE `stockin`
+  ADD PRIMARY KEY (`stockin_id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`supplier_id`);
+
+--
+-- Indexes for table `temp_trans`
+--
+ALTER TABLE `temp_trans`
+  ADD PRIMARY KEY (`temp_trans_id`);
+
+--
+-- Indexes for table `term`
+--
+ALTER TABLE `term`
+  ADD PRIMARY KEY (`term_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `branch`
+--
+ALTER TABLE `branch`
+  MODIFY `branch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `expensesinput`
+--
+ALTER TABLE `expensesinput`
+  MODIFY `expensesinput_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `history_log`
+--
+ALTER TABLE `history_log`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=210;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3275;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `purchase_request`
+--
+ALTER TABLE `purchase_request`
+  MODIFY `pr_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `sales_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=133;
+
+--
+-- AUTO_INCREMENT for table `sales_details`
+--
+ALTER TABLE `sales_details`
+  MODIFY `sales_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=133;
+
+--
+-- AUTO_INCREMENT for table `stockin`
+--
+ALTER TABLE `stockin`
+  MODIFY `stockin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `supplier_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `temp_trans`
+--
+ALTER TABLE `temp_trans`
+  MODIFY `temp_trans_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `term`
+--
+ALTER TABLE `term`
+  MODIFY `term_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
