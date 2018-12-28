cc636d9110a835b6a6e1a47bb869a019af9d368c
Emil Mendoza <milski2008@gmail.com>
2018-12-29 00:16:35 +0800

    fixed edit product

--- pages/product_update.php
+++ pages/product_update.php
@@ -6,8 +6,7 @@ endif;
 include('../dist/includes/dbcon.php');
 	$id = $_POST['id'];
 	$name =$_POST['prod_name'];
-	$supplier =$_POST['supplier'];
-	$price = 0;
+	$supplier =$_POST['supplier'];	
 	$reorder = $_POST['reorder'];
 	$category = $_POST['category'];
 	$serial = $_POST['serial'];
@@ -15,8 +14,7 @@ include('../dist/includes/dbcon.php');
 	
 	
 			
-	mysqli_query($con,"update product set prod_name='$name',base_price='$price',
-	reorder='$reorder',supplier_id='$supplier',cat_id='$category',serial='$serial',prod_desc='$desc' where prod_id='$id'")or die(mysqli_error($con));
+	mysqli_query($con,"update product set prod_name='$name',reorder='$reorder',supplier_id='$supplier',cat_id='$category',serial='$serial',prod_desc='$desc' where prod_id='$id'")or die(mysqli_error($con));
 	
 	echo "<script type='text/javascript'>alert('Successfully updated product details!');</script>";
 	echo "<script>document.location='product.php'</script>";  
