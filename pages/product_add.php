ff7a5af2cccf329f74f34cfc190fddc003e8a718
Fmatteo <markrecovery23@gmail.com>
2019-01-15 20:41:46 +0800

    wew

--- pages/product_add.php
+++ pages/product_add.php
@@ -1,31 +1,31 @@
-<?php 
+<?php
 session_start();
 $branch=$_SESSION['branch'];
 include('../dist/includes/dbcon.php');
-
-	$name = $_POST['prod_name'];
-	$price = 0;
-	$desc = $_POST['prod_desc'];
-	$supplier = $_POST['supplier'];
-	$reorder = $_POST['reorder'];
-	$category = $_POST['category'];
-	$serial = $_POST['serial'];
-	
-	$query2=mysqli_query($con,"select * from product where prod_name='$name' and branch_id='$branch'")or die(mysqli_error($con));
-		$count=mysqli_num_rows($query2);
-
-		if ($count>0)
-		{
-			echo "<script type='text/javascript'>alert('Product already exist!');</script>";
-			echo "<script>document.location='product.php'</script>";  
-		}
-		else
-		
-
-			mysqli_query($con,"INSERT INTO product(prod_name,prod_price,prod_desc,cat_id,reorder,supplier_id,branch_id,serial)
-			VALUES('$name','$price','$desc','$category','$reorder','$supplier','$branch','$serial')")or die(mysqli_error($con));
-
-			echo "<script type='text/javascript'>alert('Successfully added new product!');</script>";
-					  echo "<script>document.location='product.php'</script>";  
-		
+ 
+    $name = $_POST['prod_name'];
+    $price = 0;
+    $desc = $_POST['prod_desc'];
+    $supplier = $_POST['supplier'];
+    $reorder = $_POST['reorder'];
+    $category = $_POST['category'];
+    $serial = $_POST['serial'];
+   
+    $query2=mysqli_query($con,"select * from product where prod_name='$name' and branch_id='$branch'")or die(mysqli_error($con));
+        $count=mysqli_num_rows($query2);
+ 
+        if ($count>0)
+        {
+            echo "<script type='text/javascript'>alert('Product already exist!');</script>";
+            echo "<script>document.location='product.php'</script>";  
+        }
+        else
+       
+ 
+            mysqli_query($con,"INSERT INTO product(prod_name,prod_price,prod_desc,cat_id,reorder,supplier_id,branch_id,serial)
+            VALUES('$name','$price','$desc','$category','$reorder','$supplier','$branch','$serial')")or die(mysqli_error($con));
+ 
+            echo "<script type='text/javascript'>alert('Successfully added new product!');</script>";
+                      echo "<script>document.location='product.php'</script>";  
+       
 ?>
--- pages/stockin.php
+++ pages/stockin.php
@@ -34,7 +34,7 @@ endif;
       .main-footer  {
       display:none !important;
       }
-      div.dataTables_length label { 
+      div.dataTables_length label {
         display: none !important;
       }
       div.dataTables_filter label{
@@ -56,7 +56,7 @@ endif;
 ::-webkit-scrollbar-thumb{
   background:linear-gradient(darkred,white);
 }
-
+ 
  .sidebar {  
     width: 250;
     height:100%;
@@ -65,80 +65,80 @@ endif;
     top: 0px;
     transition: left 0.3s linear;
     }
-
+ 
     .sidebar.visible {
     left:0px;
     transition: left 0.3s linear;
     }
-
+ 
     .nav-txt {
       color: white;
     }
-
+ 
     .subnav-txt:hover {
       color: #ff0000;
     }
-
+ 
     .nav-txt:hover {
       background-color: #7d0000;
       color: white;
       transition: all .2s;
     }
-
+ 
     .main-sidebar {
       background-image: linear-gradient(to left, rgba(232,76,61,1) , rgba(193,57,43,1));
       position: fixed;
       z-index: 5;
     }
-
+ 
     .main-sidebar * a {
       color: white;
     }
-
+ 
     .treeview-menu {
       background-color: #7d0000;
     }
-
+ 
     .reorder-count {
       font-size: 10px !important;
     }
-
+ 
     .box-header {
       background-image: linear-gradient(to right, rgba(232,76,61,1) , rgba(193,57,43,1));
     }
-
+ 
     .menu {
       list-style-type: none;
       margin: 0;
       padding: 10px 15px;
     }
-
+ 
     .box-title {
       color: white;
       text-align: center;
       display: block !important;
     }
-
+ 
     .btn:hover {
       transition: all .2s linear;
     }
-
+ 
     .input-group {
       text-align: center;
       width: 100%;
     }
-
+ 
     .save-btn {
       margin: 5px;
     }
-
+ 
     .stock-btn {
       margin: 5px;
     }
     .box.box-danger{
       padding-left: 10px;
       padding-right: 10px;
-      
+     
     }
    
     </style>
@@ -162,22 +162,22 @@ endif;
               <a href="#" class="dropdown-toggle nav-txt" data-toggle="dropdown">
                       <i class="glyphicon glyphicon-refresh text-white"></i> Reorder
                       <span class="label label-success">
-                      <?php 
+                      <?php
                       $query=mysqli_query($con,"select COUNT(*) as count from product where prod_qty<=reorder and branch_id='$branch'")or die(mysqli_error());
                       $row=mysqli_fetch_array($query);
                       echo $row['count'];
                       ?>  
                       </span>
                     </a>  
-              
+             
             <li class="treeview">
               <a href="#" class="dropdown-toggle nav-txt" data-toggle="dropdown">
                       <i class="glyphicon glyphicon-wrench text-white"></i> Maintenance
-                      
+                     
                     </a>
               <ul class="treeview-menu">
        <li>
-                        
+                       
               <li><!-- start notification -->
                             <a href="category.php" class="subnav-txt">
                               <i class="glyphicon glyphicon-user text-white"></i> Company Name
@@ -208,22 +208,22 @@ endif;
                             <a href="drawing.php" class="subnav-txt">
                               <i class="glyphicon glyphicon-send text-white"></i> Drawing / Cashout
                             </a>
-                          </li><!-- end notification -->           
+                          </li><!-- end notification -->          
              <li><!-- start notification -->
                             <a href="expensesinput.php" class="subnav-txt">
                               <i class="glyphicon glyphicon-user text-white"></i> Expenses
                             </a>
                           </li><!-- end notification -->
-
+ 
                         </ul>
                       </li>
                      
-                    
+                   
                   </li>
     <li class="treeview">
       <a href="stockin.php" class="dropdown-toggle nav-txt">
                       <i class="glyphicon glyphicon-list text-white"></i> Stock in/out
-                      
+                     
                     </a>
                     <ul class="dropdown-menu">
                       <li>
@@ -265,41 +265,41 @@ endif;
                           </li><!-- end notification -->
                         </ul>
                       </li>
-                    
+                   
     <li class="treeview">
       <a href="profile.php" class="dropdown-toggle nav-txt">
                       <i class="glyphicon glyphicon-cog text-white"></i>
                       <?php echo $_SESSION['name'];?>
                     </a>
                   </li>
-
+ 
     <li class="treeview">
        <a href="logout.php" class="dropdown-toggle nav-txt">
-                      <i class="glyphicon glyphicon-off text-white"></i> Logout 
-                      
+                      <i class="glyphicon glyphicon-off text-white"></i> Logout
+                     
                     </a>
-                  </li>       
+                  </li>      
           </ul>
         </section>
         <!-- /.sidebar -->
       </aside>
-
+ 
           <!-- Content Header (Page header) -->
           <section class="content-header">
             <h1>
               <a class="btn btn-lg btn-danger" href="home.php">Back</a>
-              
+             
             </h1>
             <ol class="breadcrumb">
               <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
               <li class="active">Product</li>
             </ol>
           </section>
-
+ 
           <!-- Main content -->
           <section class="content">
             <div class="row">
-	      <div class="col-md-4">
+          <div class="col-md-4">
               <div class="box box-danger">
                 <div class="box-header">
                   <h3 class="box-title">Stockin Products</h3>
@@ -307,22 +307,22 @@ endif;
                 <div class="box-body">
                   <!-- Date range -->
                   <form method="post" action="stockin_add.php" enctype="multipart/form-data">
-  
+ 
                   <div class="form-group">
                     <label for="date">Product Name</label>
                     <div class="input-group col-md-12">
                       <select class="form-control select2" name="prod_name" id="prod_id" required>
                       <?php
-			 include('../dist/includes/dbcon.php');
-				$query2=mysqli_query($con,"select * from product where branch_id='$branch' order by prod_name")or die(mysqli_error());
-				  while($row=mysqli_fetch_array($query2)){
-		      ?>
-				    <option value="<?php echo $row['prod_id'];?>"><?php echo $row['prod_name'];?></option>
-		      <?php }?>
+             include('../dist/includes/dbcon.php');
+                $query2=mysqli_query($con,"select * from product where branch_id='$branch' order by prod_name")or die(mysqli_error());
+                  while($row=mysqli_fetch_array($query2)){
+              ?>
+                    <option value="<?php echo $row['prod_id'];?>"><?php echo $row['prod_name'];?></option>
+              <?php }?>
                     </select>
                     </div><!-- /.input group -->
                   </div><!-- /.form group -->
-		  
+         
                   <div class="form-group">
                     <label for="date">Quantity</label>
                     <div class="input-group col-md-12">
@@ -335,20 +335,20 @@ endif;
                       <input type="text" class="form-control pull-right" id="base_price" name="base_price" placeholder="Input Base Price" required>
                     </div><!-- /.input group -->
                   </div><!-- /.form group -->
-                  
+                 
                   <div class="form-group">
                     <div class="input-group">
                       <button type="submit" class="btn btn-primary save-btn" id="daterange-btn" name="">
                         Save
                       </button>
-					            <div class="btn btn-danger stockoutButton stock-btn">Stock Out</div>
+                                <div class="btn btn-danger stockoutButton stock-btn">Stock Out</div>
                     </div>
                   </div><!-- /.form group -->
-				</form>	
+                </form>
                 </div><!-- /.box-body -->
               </div><!-- /.box -->
             </div><!-- /.col (right) -->
-            
+           
             <div class="col-xs-8">
               <div class="box box-danger">
     <table id="example1" class="table table-bordered table-striped">
@@ -358,41 +358,41 @@ endif;
                 <br/>
                 <a class = "btn btn-success btn-print" href = "" onclick = "window.print()"><i class ="glyphicon glyphicon-print"></i> Print</a>
                 <div class="box-body">
-                  
+                 
                     <thead>
                       <tr>
                         <th>Product Name</th>
                         <th>Qty</th>
-				        <th>Distributor</th>
-				        <th>Date Delivered</th>
+                        <th>Distributor</th>
+                        <th>Date Delivered</th>
                       </tr>
                     </thead>
                     <tbody>
 <?php
-		$branch=$_SESSION['branch'];
-			$sql="
-			SELECT * FROM stockin a 
-			LEFT JOIN product b ON a.prod_id = b.prod_id 
-			LEFT JOIN supplier c ON b.supplier_id = c.supplier_id
-			WHERE a.branch_id='$branch'
-			order by date desc
-			";
-		
-		
-		
-		$query=mysqli_query($con,$sql)or die(mysqli_error());
-		while($row=mysqli_fetch_array($query)){
-		
+        $branch=$_SESSION['branch'];
+            $sql="
+            SELECT * FROM stockin a
+            LEFT JOIN product b ON a.prod_id = b.prod_id
+            LEFT JOIN supplier c ON b.supplier_id = c.supplier_id
+            WHERE a.branch_id='$branch'
+            order by date desc
+            ";
+       
+       
+       
+        $query=mysqli_query($con,$sql)or die(mysqli_error());
+        while($row=mysqli_fetch_array($query)){
+       
 ?>
                       <tr>
                         <td><?php echo $row['prod_name'];?></td>
                         <td><?php echo $row['qty'];?></td>
-            						<td><?php echo $row['supplier_name'];?></td>
-            						<td><?php echo $row['date'];?></td>
-                      
+                                    <td><?php echo $row['supplier_name'];?></td>
+                                    <td><?php echo $row['date'];?></td>
+                     
                       </tr>
                    
-<?php }?>					  
+<?php }?>                    
                     </tbody>
                     <tfoot>
                       <tr>
@@ -400,24 +400,24 @@ endif;
                         <th>Qty</th>
                         <th>Distributor</th>
                         <th>Date Delivered</th>
-                        
-                      </tr>					  
+                       
+                      </tr>                  
                     </tfoot>
                   </table>
                 </div><!-- /.box-body -->
  
             </div><!-- /.col -->
-			
-			
+           
+           
           </div><!-- /.row -->
-	  
-            
+     
+           
           </section><!-- /.content -->
         </div><!-- /.container -->
       </div><!-- /.content-wrapper -->
       <?php include('../dist/includes/footer.php');?>
     </div><!-- ./wrapper -->
-
+ 
     <!-- jQuery 2.1.4 -->
     <script src="../plugins/jQuery/jQuery-2.1.4.min.js"></script>
     <!-- Bootstrap 3.3.5 -->
@@ -433,7 +433,7 @@ endif;
     <script src="../dist/js/demo.js"></script>
     <script src="../plugins/datatables/jquery.dataTables.min.js"></script>
     <script src="../plugins/datatables/dataTables.bootstrap.min.js"></script>
-    
+   
     <script>
       $(function () {
         $("#example1").DataTable();
@@ -449,35 +449,35 @@ endif;
     </script>
      <script>
       $(function () {
-	
-		
-			$(".stockoutButton").click(function(e) {
+   
+       
+            $(".stockoutButton").click(function(e) {
               e.preventDefault();
               $.ajax({
                   type: "POST",
                   url: "ajax.php",
-                  data: { 
-					  prod_id: $("#prod_id").val(), // < note use of 'this' here
-					  qty: $("#qty").val(), // < note use of 'this' here
+                  data: {
+                      prod_id: $("#prod_id").val(), // < note use of 'this' here
+                      qty: $("#qty").val(), // < note use of 'this' here
                       process: 'stockout'
                   },
                   success: function(result) {
-                      if(result == ""){ 
+                      if(result == ""){
                           alert("bad")                        
                       }else{
                           alert("success")
-						  location.reload();
+                          location.reload();
                       }
-                      
+                     
                   },
                   error: function(result) {
                       alert('error');
                   }
               });
-        }); // ajax 		
-		
-		
-		
+        }); // ajax        
+       
+       
+       
       });
     </script>
   </body>
